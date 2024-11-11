715700f9f9a81334b2bed9b47154b1372678b5cd
Guimeb <126623986+Guimeb@users.noreply.github.com>
2024-11-11 00:15:24 -0300

    Update App.java

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -3,12 +3,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Grupo: Batata Doce
+ * Grupo: Repolho
  * Autores: 
- *      - João da Silva     -   jaodasilva@example.com
- *      - Jose da Silva     -   zedasilva@example.com
- *      - Maria da Silva    -   marydasilva@example.com
- *      - Monica da Silva   -   nicadasilva@example.com
+ *      - André Lambert - RM99148
+ *      - Guilherme Morais - RM551981
  */
 
 package br.com.fiap.twoespwx.libunclepresser;
